f645177b6e3425c469c4e20a00fc095f989b8174
TheBlueJeans <TheBlueJeans@users.noreply.github.com>
2016-04-19 13:22:56 +0800

    Update Qn8.py

--- Qn8.py
+++ Qn8.py
@@ -4,11 +4,10 @@ k = int(input("Enter \n"))
 array1 = [lowest] + [None]*(k-1)
 for value in A:
     if None in array1:
-        if value != lowest:
-            for i in range(len(array1)):
-                if array1[i] == None or array1[i] > value:
-                    array1 = (array1[:i] + [value] + array1[i:])[:-1]
-                    break
+        for i in range(len(array1)):
+            if array1[i] == None or array1[i] > value:
+                array1 = (array1[:i] + [value] + array1[i:])[:-1]
+                break
     elif array1[-1] > value:
         for i in range(len(array1)):
             if array1[i] > value:
