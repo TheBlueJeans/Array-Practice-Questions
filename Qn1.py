53e8608f147e8fb1cc18be21f74e734115fcffa1
TheBlueJeans <TheBlueJeans@users.noreply.github.com>
2016-04-19 12:55:05 +0800

    Update Qn1.py

--- Qn1.py
+++ Qn1.py
@@ -1,7 +1,10 @@
 count = 0
 array1 = []
+array2 = []
 for value in A:
     if value not in array1:
         array1.append(value)
-    else:
-        count += 1
+    elif value in array1:
+        if value not in array2:
+            array2.append(value)
+print(len(array2))
